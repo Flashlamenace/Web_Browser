1f60f3028cb190f2f2268c03044c8297a5d032af
Flash_La_Menace <Geodesik@Arch.net>
2025-03-12 20:22:10 +0100

    on fait un semblant de truc qui fonctionne

--- includes/network.hpp
+++ includes/network.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdlib>
 #include <iostream>
+#include <ostream>
 #include <unistd.h>
+#include <cstring>
 #include <string>
 #include <sys/socket.h>
 #include <arpa/inet.h> 
@@ -19,30 +22,18 @@ class URL{
         int         port;
     public:
         //Constructor :
-        URL(const std::string& protocol, std::string& host) : protocol(protocol), host(host) {}
+        //URL(const std::string& protocol, std::string& host) : protocol(protocol), host(host) {}
         
         //Getter :
         std::string get_protocol() const {return protocol;}
         std::string get_host    () const {return host;}
         
         //setter :
-        int set_port (int port1){return port = port1;} 
+        int set_port            (int port1)          {return port = port1;}
+        std::string set_protocol(std::string proto1) {return protocol = proto1;}
+        std::string set_host    (std::string host1)  {return host = host1;}
 
-        static URL parse_url(std::string& url){
-            size_t      position_protocol = url.find("://");
-            size_t      position_port     = url.find(":", position_protocol);
-            std::string protocol = url.substr(0, position_protocol);
-            std::string host     = url.substr(position_protocol + 3, position_port);
-
-            return URL(protocol, host);
-        }    
-
-};
-
-class PROTOCOL : URL{
-    //Prendre en compte tout le reste...
-
-    public:
+        //determine the protocol to use.
         void what_is_port_proto(){
             std::string protocol = get_protocol();
 
@@ -54,30 +45,87 @@ class PROTOCOL : URL{
                 }
 
         }
+
+        void parse_url(std::string& url){
+            size_t      position_protocol = url.find("://");
+            //size_t      position_port     = url.find(":", position_protocol);
+            std::string protocol = url.substr(0, position_protocol);
+            std::string host_and = url.substr(position_protocol + 3);
+            size_t      position_host = host_and.find("/");
+            std::string host = host_and.substr(0, position_host);
+            
+            std::cout << "protocol : " << protocol << " Host : " << host << std::endl;
+            
+            set_host    (host);
+            set_protocol(protocol);
+        }    
+
 };
+
 
 //Socket Related
 
-class SOCKET : URL{
+class SOCKET : public URL{
     private:
         int socket_fd;
+        
     public:
-        bool connect_to_server(){
+
+        int connect_to_server(){
             socket_fd = socket(AF_INET, SOCK_STREAM, 0);
             
             if (socket_fd < 0) {std::cerr << "Problem while creating socket. \n";}
 
-            struct sockaddr_in server_addr;
+            /*struct sockaddr_in server_addr;
             server_addr.sin_family = AF_INET;
             server_addr.sin_port = htons(port);
             inet_pton(AF_INET, host.c_str(), &server_addr.sin_addr);
-    
-            if (connect(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            
+            std::cout << ": " << inet_ntoa(server_addr.sin_addr) << ": " << ntohs(server_addr.sin_port) << ": " << server_addr.sin_family << std::endl; 
+            */
+
+            struct addrinfo hints, *res;
+            memset(&hints, 0, sizeof(hints));
+            hints.ai_family = AF_INET;      // Only IPv4 addresses
+            hints.ai_socktype = SOCK_STREAM;
+
+            std::cout << "host : " << host << " port : " << port << std::endl;
+        
+            int gai_result = getaddrinfo(host.c_str(), std::to_string(port).c_str(), &hints, &res);
+            if (gai_result != 0) {
+                std::cerr << "getaddrinfo failed: " << gai_strerror(gai_result) << std::endl;
+                close(socket_fd);
+                return 0;
+            }
+            if (connect(socket_fd, res->ai_addr, res->ai_addrlen) < 0) {
                 std::cerr << "Connection failed\n";
                 close(socket_fd);
-                return false;
+                freeaddrinfo(res);  // Clean up
+                return 0;
             }
-            return true;
+
+            std::cout << "Connected to " << host << " on port " << port << std::endl;
+
+            freeaddrinfo(res);
+           
+            return socket_fd;
+        }
+        
+        
+        void send_request(char *request_msg){
+        
+                send(socket_fd, request_msg, strlen(request_msg), 0); 
+        
         }
 
+        std::string receive_request(char *buffer, size_t buffer_size){    
+        
+            int sizeof_msg_received = recv(socket_fd, buffer, buffer_size, 0);
+
+            if (sizeof_msg_received > 0) {
+                buffer[sizeof_msg_received] = '\0'; // Null-terminate the received data
+            }
+        
+        return std::string(buffer);
+        }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,57 @@
-#include <iostream>
-#include <string>
+#include "../includes/network.hpp"
+
+#include <cstddef>
+#include <cstring>
+#include <ostream>
 
 int main(void){
+ 
+    std::string url = "http://exemple.org/";
+    
+    char *response;
 
+    SOCKET Socket;
 
+    const char *source = "GET / HTTP/1.0\r\nHost: example.org\r\nUser-Agent: polypous/0.1\r\n Accept: */*\r\nConnection: keep-alive\r\n\r\n";
+    size_t length = strlen(source);
+    char *request = (char *)malloc((length + 1) * sizeof(char)); // Allocate memory
 
+    if (request == nullptr) {
+        // Handle memory allocation failure
+        std::cerr << "Memory allocation failed!" << std::endl;
+        return 1;
+    }
+
+    // Copy the string into the allocated memory
+    strcpy(request, source);
+
+    char buffer[4096];
+    size_t buffer_size = 4096;
+
+    Socket.parse_url(url);
+    Socket.what_is_port_proto();
+
+    Socket.connect_to_server();
+    Socket.send_request(request);
+    
+    std::cout << Socket.receive_request(buffer, buffer_size) << std::endl << std::endl;
+
+    free(request);
+return 0;
 }
+
+//GET / HTTP/2
+//Host: exemple.org
+//User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0
+//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+//Accept-Language: en-US,en;q=0.5
+//Accept-Encoding: gzip, deflate, br, zstd
+//DNT: 1
+//Connection: keep-alive
+//Upgrade-Insecure-Requests: 1
+//Sec-Fetch-Dest: document
+//Sec-Fetch-Mode: navigate
+//Sec-Fetch-Site: none
+//Sec-Fetch-User: ?1
+//Sec-GPC: 1
+//Priority: u=0, i
